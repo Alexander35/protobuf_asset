d84349b3ef8ef523b3917d7e4606fd001618b3cc
Alexander Ivanov <alexander.ivanov.35@gmail.com>
2017-11-29 13:50:15 +0900

    msg.proto

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='protobuf_asset',
+      version='0.1',
+      description='predefined protobufer assets',
+      url='https://github.com/alexander35/protobuf_asset.git',
+      author='Alexander Ivanov',
+      author_email='alexander.ivanov.35@gmail.com',
+      license='MIT',
+      packages=['protobuf_asset'],
+      zip_safe=False)
